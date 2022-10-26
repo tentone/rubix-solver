df3a3c69242ddabf128d5faaf22039c3adc76f91
tentone <tentone@outlook.com>
2022-10-26 21:53:59 +0100

    CV

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
 #include "solver.cpp"
 #include "test.cpp"
 #include "cube.cpp"
 
 int main()
 {
+	// Read the image file
+	cv::Mat image = cv::imread("./readme/notation.png");
+
+	// Check for failure
+	if (image.empty()) 
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	std::string window = "Rubix";
+	cv::namedWindow(window);
+
+	cv::imshow(window, image);
+
+	cv::waitKey(0);
+	cv::destroyWindow(window);
+
 	// Test::test();
 
 	Cube cube = Cube();
@@ -22,7 +43,7 @@ int main()
 	// cube.scramble();
    
 	CubeSolution sol = CubeSolver::solveBF(cube);
-	std::cout << sol.solved << std::endl;
+	std::cout << sol.toString() << std::endl;
 
 	
 }
